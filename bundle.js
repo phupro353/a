8839aebf5a636193b7d93347b54966809835c388
Glitch (arras-mayhem) <none>
2020-09-05 06:37:19 +0000

    👴📮 Checkpoint ./bundle.js:15958994/2319

--- bundle.js
+++ bundle.js
@@ -434,12 +434,36 @@
                 }
                 return btoa(b).replace(/=+/, "")
             }
-      
+
       var smallaudio = new Audio();
 smallaudio.src = ("https://cloud-cube.s3.amazonaws.com/m660o440l0wv/public/sysse_ok.ogg");
 function PlaySound210() {
     smallaudio.play();
 }
+      
+            var smallaudio2 = new Audio();
+smallaudio2.src = ("https://cloud-cube.s3.amazonaws.com/m660o440l0wv/public/sysse_ok.ogg");
+function PlaySound220() {
+    smallaudio2.play();
+}
+        
+            
+    var result = document.querySelectorAll('button')
+var step = 0
+document.querySelectorAll('button')('click', function (event) {
+  // `0` is false-y in JS, so if `step === 0`, then it'll be set to 3
+  // otherwise, it'll be set to 0 (`3` is truth-y)
+  step = step ? 0 : 3
+  result.innerHTML = 'step is ' + step
+  
+  // if you want to do something with `step`:
+  if (step === 0) {
+    smallaudio.play();
+  } else {
+    smallaudio2.play();
+  }
+})
+      
 
             function t() {
                 if (!ua) {
