fb21c4b2c37723d78638393286fea6c9d3836c51
Glitch (arras-mayhem) <none>
2020-09-05 07:03:44 +0000

    Rewound to commit 63269f108ef63ddcb197abd6c60d825d35580b7b + 0 edits

--- bundle.js
+++ bundle.js
@@ -434,36 +434,12 @@
                 }
                 return btoa(b).replace(/=+/, "")
             }
-
+      
       var smallaudio = new Audio();
 smallaudio.src = ("https://cloud-cube.s3.amazonaws.com/m660o440l0wv/public/sysse_ok.ogg");
 function PlaySound210() {
     smallaudio.play();
 }
-      
-            var smallaudio2 = new Audio();
-smallaudio2.src = ("https://cloud-cube.s3.amazonaws.com/m660o440l0wv/public/cancel.wav");
-function PlaySound220() {
-    smallaudio2.play();
-}
-      
-var result = document.getElementById('viewOptionText')
-var step = 0
-document.getElementById('viewOptionText').addEventListener('click', function (event) {
-  // `0` is false-y in JS, so if `step === 0`, then it'll be set to 3
-  // otherwise, it'll be set to 0 (`3` is truth-y)
-  step = step ? 0 : 3
-  result.innerHTML = 'step is ' + step
-  
-  // if you want to do something with `step`:
-  if (step === 0) {
-    smallaudio2.play();
-  } else {
-    smallaudio.play();
-  }
-})
-      
-        
 
             function t() {
                 if (!ua) {
