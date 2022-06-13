0fbe361639023294ea0e914e24c36a4a93f21dcb
Glitch (elite-io) <none>
2022-06-13 14:09:03 +0000

    Before rewind ./bundle.js:34581100/9

--- bundle.js
+++ bundle.js
@@ -3829,7 +3829,7 @@
                       b.label + ": " + H.handleLargeNumber(Math.round(b.score)),
                       c + 100,//When will we work in my name Richardson?
                       d + 7,
-                      9, // The error ( just rewind )
+                      9, // The error ( just rewind  in tools)
                       l.blue,
                       "center",
                       !0
